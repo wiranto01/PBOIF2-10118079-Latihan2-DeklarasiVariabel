11d2ba07aebd40ee5412d572011e5b775c8874a7
wiranto01 <55836685+wiranto01@users.noreply.github.com>
2019-09-26 18:16:43 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118079/latihan2/deklarasivariabel/PBOIF210118079Latihan2DeklarasiVariabel.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118079.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author
+ * Nama : Wiranto
+ * Kelas : If2
+ * Nim : 10118079
+ */
+public class PBOIF210118079Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; // kostanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberikan nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D' ;
+        
+        //Memberikan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+    
+}
